7b4d893cdf453d40f96af251cd900fd9c35bea42
dalma belizan <dall.belizan@gmail.com>
2022-07-28 20:37:35 -0300

    first commit

---
+++ jarvis/archivo.js
@@ -0,0 +1,42 @@
+
+
+let bienvenida = prompt("¿Cómo te llamas?")
+if (true) {
+    console.log("Hola " + bienvenida + "!")
+
+}
+
+let respuesta = parseInt(prompt("Cuenta regresiva en...(Del 3 hasta el 6 por favor)"))
+
+
+while (respuesta >= 7) {
+    alert("Ingresaste " + respuesta)
+    alert("MENOS de 6 te dije!")
+    respuesta = parseInt(prompt("UN NUMERO MENOR A 6"))
+}
+
+while (respuesta <= 2) {
+    alert("Ingresaste " + respuesta,)
+    alert("MAYOR a 3 te dije!")
+    respuesta = parseInt(prompt("UN NUMERO MAYOR A 3"))
+}
+
+
+{
+    for (respuesta; respuesta >= 1; respuesta--)
+
+        alert(respuesta);
+    while (respuesta != 0)
+        break;
+    alert(respuesta);
+
+    console.warn("Ahora sí!");
+}
+
+
+
+
+
+
+//parseInt(prompt ("Cuenta regresiva en...(Del 3 hasta el 10 por favor)"))
+//alert parseInt(prompt())
